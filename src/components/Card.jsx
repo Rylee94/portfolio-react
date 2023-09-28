257121722908800ea290b2f6c4c8aa820fb2d556
Rylee94 <128653866+Rylee94@users.noreply.github.com>
2023-09-28 18:33:17 -0500

    more styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faGithub
-} from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function PortfolioCard(props) {
-    return (
-        <>
-            <div className="card">
-                <img className="card-image" src={props.image} alt="" />
-                <h2 className="card-name">{props.name}</h2>
-                <div className="card-link">
-                    <a href={props.ProjectURL} className="project-link">
-                    </a>
-                    <a href={props.gitHubURL} className="project-link-github">
-                        <FontAwesomeIcon icon={faGithub} size="3x" />
-                    </a>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <div className="card">
+      <div className="card-image-container">
+        <img className="card-image" src={props.image} alt="" />
+        <a href={props.gitHubURL} className="project-link-github">
+          <FontAwesomeIcon icon={faGithub} size="3x" />
+        </a>
+      </div>
+      <h2 className="card-name">{props.name}</h2>
+      <div className="card-link">
+        <a href={props.ProjectURL} className="project-link"></a>
+      </div>
+    </div>
+  );
 }
+
