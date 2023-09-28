e963f44f52e3e09a1107c8c715a52eb5221e1951
Rylee94 <128653866+Rylee94@users.noreply.github.com>
2023-09-28 18:08:44 -0500

    start styling portfolio page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faGithub
+} from "@fortawesome/free-brands-svg-icons";
+
 export default function PortfolioCard(props) {
     return (
         <>
             <div className="card">
-                <img className='card-image' src={props.image} alt="" />
-                <h2 className='card-title'>{props.name}</h2>
-                <div className="card-links">
-                    <a href={props.ProjectURL} className="card-link-deployed"> <img src="" alt="" /></a>
-                    <a href={props.gitHubURL} className="card-link-github"> <img src="" alt="" /></a>
+                <img className="card-image" src={props.image} alt="" />
+                <h2 className="card-name">{props.name}</h2>
+                <div className="card-link">
+                    <a href={props.ProjectURL} className="project-link">
+                    </a>
+                    <a href={props.gitHubURL} className="project-link-github">
+                        <FontAwesomeIcon icon={faGithub} size="3x" />
+                    </a>
                 </div>
             </div>
         </>
-    )
+    );
 }
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,53 +1,37 @@
-import PortfolioCard from '../components/Card'
+import PortfolioCard from '../components/Card';
 
 export default function Portfolio() {
-    return (<>
-        <div>
-            <h2 className='title'>Portfolio</h2>
-        </div>
-        <div>
-            <PortfolioCard
-                image={'./assets/project1.jpg'}
-                name={'Text Editor'}
-                ProjectURL={'https://blooming-sands-12028-8c55ff99bfb0.herokuapp.com/'}
-                gitHubURL={'https://github.com/Rylee94/PWA-TextEditor'}
-
-            />
-            <PortfolioCard
-                image={'./assets/project2.jpg'}
-                name={'Coding Quiz'}
-                ProjectURL={'https://rylee94.github.io/javascript-quiz-2/'}
-                gitHubURL={'https://github.com/Rylee94/javascript-quiz-2'}
-
-            />
-            {/* <PortfolioCard
-                image={ }
-                name={ }
-                Pr0jectURL={ }
-                gitHubURL
-
-            />
-            <PortfolioCard
-                image={ }
-                name={ }
-                Pr0jectURL={ }
-                gitHubURL
-
-            />
-            <PortfolioCard
-                image={ }
-                name={ }
-                Pr0jectURL={ }
-                gitHubURL
-
-            />
-            <PortfolioCard
-                image={ }
-                name={ }
-                Pr0jectURL={ }
-                gitHubURL
-
-            /> */}
-        </div>
-    </>)
+    return (
+        <>
+            <div>
+                <h2 className='title'>Portfolio</h2>
+            </div>
+            <div className='cards'>
+                <PortfolioCard
+                    image={'public/assets/project1.png'}
+                    name={'Text Editor'}
+                    ProjectURL={'https://blooming-sands-12028-8c55ff99bfb0.herokuapp.com/'}
+                    gitHubURL={'https://github.com/Rylee94/PWA-TextEditor'}
+                />
+                <PortfolioCard
+                    image={'public/assets/project2.png'}
+                    name={'Coding Quiz'}
+                    ProjectURL={'https://rylee94.github.io/javascript-quiz-2/'}
+                    gitHubURL={'https://github.com/Rylee94/javascript-quiz-2'}
+                />
+                <PortfolioCard
+                    image={'public/assets/project3.png'}
+                    name={'OnlyGigs'}
+                    ProjectURL={'https://sheltered-inlet-74192-222de79efe22.herokuapp.com/'}
+                    gitHubURL={'https://github.com/BenWhann/OnlyGigs'}
+                />
+                <PortfolioCard
+                    image={'public/assets/project4.png'}
+                    name={'Note Taker'}
+                    ProjectURL={'https://rylees-notebook-738088f4224f.herokuapp.com/'}
+                    gitHubURL={'https://github.com/Rylee94/express-note-taker'}
+                />
+            </div>
+        </>
+    );
 }
